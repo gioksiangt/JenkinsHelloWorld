0545e9492bd9ee4086bfd633d41d47449c1c8be9
gioksiangt <47885681+gioksiangt@users.noreply.github.com>
2020-06-10 20:39:20 +0800

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testing");
+		System.out.println(" **** Testing some change****");
 	}
 
 }
